e0fa528b4fb85e10143cdad4240981f8236fb930
frontend.dev.pm@gmail.com <frontend.dev.pm@gmail.com>
2021-12-14 13:04:18 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# To-do-list-app
+
+# Description
+
+I would like to introduce my new project. This is a to-do list application with which you can create your own to-do list. You can add new tasks, mark completed tasks, delete them one by one or delete all tasks at once.
+
+![Animation](https://github.com/MieczyslawFrontDev/to-do-list-app/blob/main/sources/to-do%20list%20animation.gif?raw=true)
+
+# Demo
+
+https://mieczyslawfrontdev.github.io/to-do-list-app/
+
+# Technologies
+
+1. HTML
+2. CSS
+3. CSS Grid
+4. BEM
+5. Media queries
+6. JavaScript# 
---
+++ script.js
@@ -0,0 +1,104 @@
+{
+    const tasks = [];
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    }
+
+    const removeAllTask = () => {
+        tasks.splice(0, tasks.length);
+        render();
+    }
+
+    const addNewTask = (newTaskContent) => {
+
+        tasks.push({
+            content: newTaskContent,
+        })
+        render();
+    };
+
+    const toggleTaskDone = (taskIndex) => {
+        tasks[taskIndex].done = !tasks[taskIndex].done
+        render();
+    }
+
+    const render = () => {
+
+        let htmlString = "";
+
+        for (const task of tasks) {
+
+            htmlString += `
+
+                <li class="tasksList__item"
+                >
+                <button class="tasksList__button tasksList__button--done js-done">${task.done ? "✓" : ""}</button>
+                <span class="${task.done ? "js-item--done" : ""}">${task.content}</span>
+                <button class="tasksList__button tasksList__button--remove js-remove">🗑</button>
+                </li>`
+        };
+
+        document.querySelector(".js-tasksList").innerHTML = htmlString;
+
+        bindEvents();
+    };
+
+    const bindEvents = () => {
+
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
+        });
+
+        const toggleDoneButtons = document.querySelectorAll(".js-done");
+
+        toggleDoneButtons.forEach((toggleDoneButton, index) => {
+            toggleDoneButton.addEventListener("click", () => {
+                toggleTaskDone(index);
+            })
+        });
+
+        const removeAll = document.querySelector(".js-removeAll");
+
+        removeAll.addEventListener("click", removeAllTask);
+    }
+
+    const clearNewTaskContent = (taskInput) => {
+
+        taskInput.value = "";
+        taskInput.focus();
+
+    }
+
+    const onFormSubmit = (e) => {
+        e.preventDefault();
+
+        const taskInput = document.querySelector(".js-newTask");
+        const newTaskContent = taskInput.value.trim();
+
+        if (newTaskContent === "") {
+            taskInput.focus();
+
+        } else {
+            addNewTask(newTaskContent);
+        }
+        clearNewTaskContent(taskInput)
+
+    }
+
+    const init = () => {
+        render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
+    };
+
+    init();
+
+};
